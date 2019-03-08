cd97b8a7985bd7425618fb03dfba4da749b3cf5f
BazarovaAnna <40502319+BazarovaAnna@users.noreply.github.com>
2019-03-09 00:31:48 +0300

    Delete db.sql

--- db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE results (
-    id bigserial primary key,
-    x double precision not null,
-    y double precision not null,
-    r double precision not null,
-    checking boolean not null,
-    sessionID varchar(100) not null
-);
